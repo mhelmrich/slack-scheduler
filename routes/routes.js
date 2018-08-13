4f9672e368974fc65473232d71164bbce7978bd3
Kitan Garcia <kitangar@gmail.com>
2018-08-13 15:11:19 -0700

    can now successfully route to /oauthcallback and log token info

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {google} = require('googleapis')
 var router = express.Router();
 
-router.get("/", function(req, res, next)
-{
-  console.log("Tdfijsfojds");
-});
-
+const oauth2Client = new google.auth.OAuth2(
+ process.env.CLIENT_ID,
+ process.env.CLIENT_SECRET,
+ process.env.REDIRECT_URL
+)
 
 router.get("/oauthcallback", function(req, res)
 {
-  console.log(req.body);
+
+  oauth2Client.getToken(req.query.code, function (err, token) {
+    if (err) return console.error(err.message)
+    {
+      console.log('token', token);
+      console.log('\n');
+      console.log('req.query:', req.query) // req.query.state <- meta-data
+    }
+    res.send('Congratulations! You have successfully linked your Google Calendar!')
+  });
 });
 
 
