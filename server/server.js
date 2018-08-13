9a0bfa531f435f528697cef137ac31491fbe2641
Kitan Garcia <kitangar@gmail.com>
2018-08-13 13:25:42 -0700

    resolve merge conflict with server.js and package.json

--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD:server.js
 const { WebClient, RTMClient } = require('@slack/client');
 const {google} = require('googleapis')
 const express = require('express')
 const bodyParser = require('body-parser')
-=======
-const { WebClient } = require('@slack/client');
 var Task = require("./models/task.js");
->>>>>>> fd3fbab00ae0b3028cf9591ccdad65c64410b3c6:server/server.js
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
 const token = process.env.SLACK_TOKEN;
@@ -15,13 +11,9 @@ const web = new WebClient(token);
 const rtm = new RTMClient(token)
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-<<<<<<< HEAD:server.js
-const conversationId = 'DC7KGLWAX';
 
 rtm.start();
-=======
 const conversationId = 'DC7KL23U3';
->>>>>>> fd3fbab00ae0b3028cf9591ccdad65c64410b3c6:server/server.js
 
 // See: https://api.slack.com/methods/chat.postMessage
 /*web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
