cbc096a018acf9cb0cf12108431abd5849ebb2c5
Diego Ibarra <dibarra@uchicago.edu>
2018-08-13 13:15:53 -0700

    Leveraged Real Time Messaging API to listen for user messages in server.js

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
-const { WebClient } = require('@slack/client');
+const { WebClient, RTMClient } = require('@slack/client');
+const {google} = require('googleapis')
+const express = require('express')
+const bodyParser = require('body-parser')
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
 const token = process.env.SLACK_TOKEN;
 
 const web = new WebClient(token);
+const rtm = new RTMClient(token)
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = 'DC74SU5A5';
+const conversationId = 'DC7KGLWAX';
+
+rtm.start();
 
 // See: https://api.slack.com/methods/chat.postMessage
-web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
+/*web.chat.postMessage({ channel: conversationId, text: 'Hello there' })
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);*/
+
+rtm.sendMessage('Hello there', conversationId)
   .then((res) => {
     // `res` contains information about the posted message
     console.log('Message sent: ', res.ts);
   })
   .catch(console.error);
+
+  rtm.on('message', (event) => {
+    // For structure of `event`, see https://api.slack.com/events/message
+    console.log(event.text)
+    var echo = event.text;
+    if(echo === 'Marco') {
+      echo = 'Polo'
+    }
+    rtm.sendMessage(echo, conversationId)
+      .then((res) => {
+        // `res` contains information about the posted message
+        console.log('Message sent: ', res.ts);
+      })
+      .catch(console.error);
+  });
